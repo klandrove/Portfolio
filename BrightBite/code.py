edfceebc6299f190db2d72e782defba99c03db26
klandrove <81109745+klandrove@users.noreply.github.com>
2024-11-11 16:00:58 -0500

    Update code.py

--- BrightBite/code.py
+++ BrightBite/code.py
@@ -1,9 +1,11 @@
+# import the necessary libraries
 import board
 import pwmio
 from adafruit_motor import servo
 import time
 import analogio
 
+# create an analog input object for the photocell
 photocell = analogio.AnalogIn(board.A3)
 
 # create PWMOut objects
@@ -14,27 +16,36 @@ pwm2 = pwmio.PWMOut(board.A2, duty_cycle=2 ** 15, frequency=50)
 top_servo = servo.Servo(pwm)
 bottom_servo = servo.Servo(pwm2)
 
+# create a variable to store the state of the scoop
 scoop = False
 
 while True:
 
     time.sleep(1)
+    # read the value of the photocell
     potVoltage = photocell.value
-    print("Analog Voltage:" + str(potVoltage))
-    print(str(scoop) + "??")
 
-    if potVoltage>20000: #when the timer goes off save a timestamp of when that happened
+    # check if the value of the photocell > 20000
+    # this indicates that the light is on
+    if potVoltage>20000:
         # check the value of scoop, if scoop equals false
         if scoop == False:
+            # set scoop to True
             scoop = True
+            # move the top servo to 180 degrees
             top_servo.angle = 180
             time.sleep(1)
+            # move the bottom servo to 150 degrees
             bottom_servo.angle = 150
             time.sleep(1)
+            # move the top servo to 0 degrees
             top_servo.angle = 0
             time.sleep(1)
+            # move the bottom servo to 30 degrees
             bottom_servo.angle = 30
 
+    # check if the value of the photocell < 3000
+    # this indicates that the light is off
     if potVoltage<3000:
         scoop = False
 
